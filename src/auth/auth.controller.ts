703901b959f45d8f53300d63bba4492d173c90bc
abrambagus <you@example.com>
2021-12-17 20:05:25 +0700

    16-commit

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,12 +1,16 @@
-import { BadRequestException, Body, Controller, Post } from '@nestjs/common';
+import { BadRequestException, Body, ClassSerializerInterceptor, Controller, Get, NotFoundException, Post, Req, Res, UseGuards, UseInterceptors } from '@nestjs/common';
 import { UserService } from 'src/user/user.service';
 import * as bcrypt from 'bcryptjs'
 import { RegisterDto } from './models/register.dto';
+import { JwtService } from '@nestjs/jwt';
+import { Request, Response} from 'express'
+import { AuthGuard } from './auth.guard';
 
+@UseInterceptors(ClassSerializerInterceptor)
 @Controller()
 export class AuthController {
 
-    constructor(private userService: UserService){
+    constructor(private userService: UserService, private jwtService: JwtService){
 
     }
 
@@ -24,6 +28,49 @@ export class AuthController {
             email: body.email,
             password: hashed
         })
+    }
+
+    @Post('login')
+    async login(
+        @Body('email') email: string, 
+        @Body('password') password: string,
+        @Res({passthrough: true}) response: Response
+        ){
+        const user = await this.userService.findOne({email})
+
+        if(!user){
+            throw new NotFoundException('User not found')
+        }
+
+        if(!await bcrypt.compare(password, user.password)){
+            throw new BadRequestException('Invalid credential')
+        }
+
+        const jwt = await this.jwtService.signAsync({id: user.id})
+
+        response.cookie('jwt', jwt, {httpOnly:true})
+        return user
+    }
+
+
+    @UseGuards(AuthGuard)
+    @Get('user')
+    async user(@Req() request: Request){
+        const cookie = request.cookies['jwt']
+
+        const data = await this.jwtService.verifyAsync(cookie)
+
+        return this.userService.findOne({id: data['id']})
+    }
+
+    @UseGuards(AuthGuard)
+    @Post('logout')
+    async logout(@Res({passthrough: true}) response: Response){
+        response.clearCookie('jwt')
+
+        return {
+            message: 'Success'
+        }
     }
 
 }
--- src/user/models/user.entity.ts
+++ src/user/models/user.entity.ts
@@ -1,3 +1,4 @@
+import { Exclude } from "class-transformer";
 import { Column, Entity, PrimaryGeneratedColumn } from "typeorm";
 @Entity('users')
 export class User {
@@ -14,5 +15,6 @@ export class User {
     email: string
 
     @Column()
+    @Exclude()
     password: string
 }
